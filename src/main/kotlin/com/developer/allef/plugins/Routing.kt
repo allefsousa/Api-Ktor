f2e2ec141187884d6866f5b9115941547d5e8684
Allef Sousa <allefsousa_1@hotmail.com>
2023-05-08 00:26:36 -0300

    Query params

--- src/main/kotlin/com/developer/allef/plugins/Routing.kt
+++ src/main/kotlin/com/developer/allef/plugins/Routing.kt
@@ -3,10 +3,16 @@ package com.developer.allef.plugins
 import io.ktor.server.routing.*
 import io.ktor.server.response.*
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 
 fun Application.configureRouting() {
     routing {
         get("/") {
+            println("URI: ${call.request.uri}")
+            println("Headers: ${call.request.headers.names()}")
+            println("Query Params ${call.request.queryParameters.names()}")
+            println("Query Params ${call.request.queryParameters.names()}")
+            println("Query Params ${call.request.queryParameters["name"]}")
             call.respondText("Hello World!")
             call.application
         }
